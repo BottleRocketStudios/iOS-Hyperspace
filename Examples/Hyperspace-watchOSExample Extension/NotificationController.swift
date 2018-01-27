77b98579e7bf6cd9e80ba10a7aaf5042a3dd9467
Tyler Milner <tyler.milner@bottlerocketstudios.com>
2018-01-27 14:56:12 -0600

    Re-implemented watchOS example ("Hyperspace-watchOSExample" target).

---
+++ Examples/Hyperspace-watchOSExample Extension/NotificationController.swift
@@ -0,0 +1,15 @@
+//
+//  NotificationController.swift
+//  Hyperspace-watchOSExample Extension
+//
+//  Created by Tyler Milner on 1/27/18.
+//  Copyright © 2018 Bottle Rocket Studios. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import UserNotifications
+
+
+class NotificationController: WKUserNotificationInterfaceController {
+}
