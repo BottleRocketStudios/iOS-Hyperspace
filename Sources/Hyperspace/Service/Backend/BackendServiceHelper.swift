b5bac151fd5c328269d9e0b8d8d8871bec13f860
Earl Gaspard <earl.gaspard@bottlerocketstudios.com>
2018-05-09 15:24:07 -0500

    Fixed comment description.

--- Sources/Hyperspace/Service/Backend/BackendServiceHelper.swift
+++ Sources/Hyperspace/Service/Backend/BackendServiceHelper.swift
@@ -15,7 +15,7 @@ public struct BackendServiceHelper {
     ///
     /// - Parameters:
     ///   - data: The raw Data retrieved from the network.
-    ///   - request: The NetworkRequest to be executed.
+    ///   - request: The NetworkRequest that will be used to transform the Data.
     ///   - completion: The completion block to invoke when execution has finished.
     public static func handleResponseData<T: NetworkRequest>(_ data: Data, for request: T, completion: @escaping BackendServiceCompletion<T.ResponseType>) {
         let transformResult = request.transformData(data)
