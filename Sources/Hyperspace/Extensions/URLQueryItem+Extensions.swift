5759cc4646c8d01fe7e813f52d5b606028976100
Will McGinty <will.mcginty@bottlerocketstudios.com>
2018-05-17 11:59:13 -0500

    Some access control tweaks

--- Sources/Hyperspace/Extensions/URLQueryItem+Extensions.swift
+++ Sources/Hyperspace/Extensions/URLQueryItem+Extensions.swift
@@ -8,8 +8,9 @@
 
 import Foundation
 
-extension URLQueryItem {
-    public static func generateRawQueryParametersString(from queryParameters: [URLQueryItem]) -> String {
+public extension URLQueryItem {
+    
+    static func generateRawQueryParametersString(from queryParameters: [URLQueryItem]) -> String {
         // Start with the empty string, appending "&<key>=<value>" for each key-value pair (except the first, which doesn't have the '&' suffix).
         return queryParameters.reduce("") { (partialResult, queryItem) -> String in
             let nextPartialResult = (partialResult.isEmpty ? "" : "\(partialResult)&")
