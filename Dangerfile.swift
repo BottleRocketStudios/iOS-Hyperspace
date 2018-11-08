122f5b75c6edac743a4ff3ba33f77227579f05fb
Tyler Milner <tyler.milner@bottlerocketstudios.com>
2018-11-08 17:00:39 -0600

    Integrated Danger into Travis-CI based on https://danger.systems/js/swift.html.

---
+++ Dangerfile.swift
@@ -0,0 +1,10 @@
+import Danger
+
+let allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
+
+let changelogChanged = allSourceFiles.contains("CHANGELOG.md")
+let sourceChanges = allSourceFiles.first(where: { $0.hasPrefix("Sources") })
+
+if !changelogChanged && sourceChanges != nil {
+    warn("No CHANGELOG entry added.")
+}
