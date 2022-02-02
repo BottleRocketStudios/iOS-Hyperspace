6532e8abfe246ffe5343f26809ec4ab201c5b518
Andrew Winn <andrew.winn@bottlerocketstudios.com>
2022-02-02 15:50:20 -0600

    Add macOS AppKit example target

---
+++ Example/Sources/macOS Example/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  macOS Example
+//
+//  Created by Andrew Winn on 2/2/22.
+//  Copyright © 2022 Bottle Rocket Studios. All rights reserved.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Example/Sources/macOS Example/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macOS Example
+//
+//  Created by Andrew Winn on 2/2/22.
+//  Copyright © 2022 Bottle Rocket Studios. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
