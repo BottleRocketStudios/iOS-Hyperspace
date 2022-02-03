bb622ee78d61ee6bba64a2de8bab5c1e93aea6c2
Andrew Winn <andrew.winn@bottlerocketstudios.com>
2022-02-03 11:54:01 -0600

    Create AppKit Version

--- Example/Sources/macOS/ViewController.swift
+++ Example/Sources/macOS/ViewController.swift
@@ -7,21 +7,90 @@
 //
 
 import Cocoa
+import Hyperspace
 
 class ViewController: NSViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    // MARK: - IBOutlets
+    @IBOutlet private var postTextField: NSTextField!
 
-        // Do any additional setup after loading the view.
+    // MARK: - Properties
+    private let backendService = BackendService()
+
+    // MARK: - IBActions
+    @IBAction private func getUserButtonTapped(_ sender: NSButton) {
+        getUser()
     }
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    @IBAction private func createPostButtonTapped(_ sender: NSButton) {
+        let stringValue = postTextField.stringValue
+        let title = stringValue.isEmpty ? "<no title>" : stringValue
+        createPost(titled: title)
+    }
+
+    @IBAction private func deletePostButtonTapped(_ sender: NSButton) {
+        deletePost(postId: 1)
+    }
+}
+
+extension ViewController {
+    private func getUser() {
+        let getUserRequest = Request<User, AnyError>.getUser(withID: 1)
+
+        backendService.execute(request: getUserRequest) { [weak self] result in
+            debugPrint("Get user result: \(result)")
+
+            switch result {
+            case .success(let user):
+                self?.presentAlert(titled: "Fetched user", message: "\(user)")
+            case .failure(let error):
+                self?.presentAlert(titled: "Error", message: "\(error)")
+            }
         }
     }
 
+    private func createPost(titled title: String) {
+        let post = NewPost(userId: 1, title: title, body: "")
+        let createPostRequest = Request<Post, AnyError>.createPost(post)
 
+        backendService.execute(request: createPostRequest) { [weak self] result in
+            debugPrint("Create post result: \(result)")
+
+            switch result {
+            case .success(let post):
+                self?.presentAlert(titled: "Created post", message: "\(post)")
+            case .failure(let error):
+                self?.presentAlert(titled: "Error", message: "\(error)")
+            }
+        }
+    }
+
+    private func deletePost(postId: Int) {
+        let deletePostRequest = Request<EmptyResponse, AnyError>.deletePost(withID: postId)
+
+        backendService.execute(request: deletePostRequest) { [weak self] result in
+            switch result {
+            case .success:
+                self?.presentAlert(titled: "Deleted Post", message: "Success")
+            case .failure(let error):
+                self?.presentAlert(titled: "Error", message: "\(error)")
+            }
+        }
+    }
 }
 
+// MARK: - Helper
+
+extension NSViewController {
+
+    func presentAlert(titled title: String, message: String) {
+        let alertView = NSAlert()
+        alertView.alertStyle = .informational
+        alertView.accessoryView = NSView(frame: CGRect(x: 0, y: 0, width: 300, height: 0))
+
+        alertView.addButton(withTitle: "OK")
+        alertView.messageText = title
+        alertView.informativeText = message
+        alertView.runModal()
+    }
+}
