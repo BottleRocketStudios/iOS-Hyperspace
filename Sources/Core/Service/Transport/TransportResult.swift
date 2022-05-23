11989a2c8e409d2630dbc000a4ebe7c4d5c0f158
Will McGinty <will.mcginty@bottlerocketstudios.com>
2022-05-23 18:25:59 -0400

    Cleanup in TransportFailure

--- Sources/Core/Service/Transport/TransportResult.swift
+++ Sources/Core/Service/Transport/TransportResult.swift
@@ -7,31 +7,6 @@
 
 import Foundation
 
-/// Represents an error that occurred when executing a `Request` using a `TransportService`.
-public struct TransportError: Error, Equatable {
-    
-    // MARK: - Code Subtype
-    public enum Code: Equatable {
-        case redirection
-        case clientError(HTTP.Status.ClientError)
-        case serverError(HTTP.Status.ServerError)
-        case unknown
-    }
-    
-    // MARK: - Properties
-    public let code: Code
-
-    // MARK: - Initializers
-    public init(code: Code) {
-        self.code = code
-    }
-
-    // MARK: - Equatable
-    public static func == (lhs: TransportError, rhs: TransportError) -> Bool {
-        return lhs.code == rhs.code
-    }
-}
-
 /// Represents the successful result of executing a `Request` using a `TransportService`.
 public struct TransportSuccess: Equatable {
 
@@ -50,20 +25,28 @@ public struct TransportSuccess: Equatable {
 /// Represents the failed result of executing a `Request` using a `TransportService`.
 public struct TransportFailure: Error, Equatable {
 
+    // MARK: - Kind Subtype
+    public enum Kind: Equatable {
+        case redirection
+        case clientError(HTTP.Status.ClientError)
+        case serverError(HTTP.Status.ServerError)
+        case unknown
+    }
+
     // MARK: - Properties
-    public let error: TransportError
+    public let kind: Kind
     public let request: HTTP.Request
     public let response: HTTP.Response?
 
     // MARK: - Initializers
-    public init(error: TransportError, request: HTTP.Request, response: HTTP.Response? = nil) {
-        self.error = error
+    public init(kind: Kind, request: HTTP.Request, response: HTTP.Response? = nil) {
+        self.kind = kind
         self.request = request
         self.response = response
     }
 
-    public init(error: TransportError, response: HTTP.Response) {
-        self.init(error: error, request: response.request, response: response)
+    public init(kind: Kind, response: HTTP.Response) {
+        self.init(kind: kind, request: response.request, response: response)
     }
 }
 
@@ -95,10 +78,10 @@ public extension HTTP.Response {
     var transportResult: TransportResult {
         switch status {
         case .success: return .success(TransportSuccess(response: self))
-        case .redirection: return .failure(TransportFailure(error: TransportError(code: .redirection), response: self))
-        case .clientError(let clientError): return .failure(TransportFailure(error: TransportError(code: .clientError(clientError)), response: self))
-        case .serverError(let serverError): return .failure(TransportFailure(error: TransportError(code: .serverError(serverError)), response: self))
-        case .unknown: return .failure(TransportFailure(error: TransportError(code: .unknown), response: self))
+        case .redirection: return .failure(TransportFailure(kind: .redirection, response: self))
+        case .clientError(let clientError): return .failure(TransportFailure(kind: .clientError(clientError), response: self))
+        case .serverError(let serverError): return .failure(TransportFailure(kind: .serverError(serverError), response: self))
+        case .unknown: return .failure(TransportFailure(kind: .unknown, response: self))
         }
     }
 }
