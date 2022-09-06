1eda5b27b2eb32a1e9b19405b7b845f0ca285f71
Daniel Larsen <daniel.larsen@bottlerocketstudios.com>
2022-09-06 16:39:07 -0500

    Added back backend tests, even though they’re commented out for now

---
+++ Tests/Helper/Mocks/TransportServiceMockClass.swift
@@ -0,0 +1,19 @@
+//
+//  TransportServiceMockSubclass.swift
+//  Tests
+//
+//  Copyright © 2018 Bottle Rocket Studios. All rights reserved.
+//
+
+//import Foundation
+//@testable import Hyperspace
+//
+//actor TransportServiceMockSubclass: TransportService {
+//
+//    private(set) var initWithNetworkActivityControllerCalled = false
+//
+//    override init(session: TransportSession = URLSession.shared, networkActivityIndicatable: NetworkActivityIndicatable? = nil) {
+//        initWithNetworkActivityControllerCalled = true
+//        super.init(session: session, networkActivityIndicatable: networkActivityIndicatable)
+//    }
+//}
