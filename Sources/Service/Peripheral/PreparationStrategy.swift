0f2e24e8070efabf04fde8fcf888d48ea407d877
Will McGinty <will.mcginty@bottlerocketstudios.com>
2023-07-27 10:59:36 -0500

    Fix a typo

--- Sources/Service/Peripheral/PreparationStrategy.swift
+++ Sources/Service/Peripheral/PreparationStrategy.swift
@@ -14,6 +14,6 @@ public protocol PreparationStrategy {
     /// - Parameter request: The request that is about to be executed
     /// - Returns: A modified version of the `Request` that will be executed.
     ///
-    ///  While this method is both `async` and `throws`, any errors thrown as part of the preparation are not recoverable using any of the`RecoveryStrategy` attached to the executing `BackendService`.
+    ///  While this method is both `async` and `throws`, any errors thrown as part of the preparation are not recoverable using any of the `RecoveryStrategy` attached to the executing `BackendService`.
     func prepare<R>(toExecute request: Request<R>) async throws -> Request<R>
 }
