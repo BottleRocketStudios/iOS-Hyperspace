f6868262c6263703983e2bbf67bd139f9c41c851
Will McGinty <will.mcginty@bottlerocketstudios.com>
2025-01-28 14:35:34 -0600

    Move Recoverable to HyperspaceTypes

--- Sources/Service/Peripheral/RecoveryStrategy.swift
+++ Sources/Service/Peripheral/RecoveryStrategy.swift
@@ -7,37 +7,6 @@
 
 import Foundation
 
-// MARK: - Recoverable
-
-/// Represents an operation that can be attempted multiple times in the event of failure.
-public protocol Recoverable {
-    
-    /// The number of recovery attempts that this operation has made
-    var recoveryAttemptCount: UInt { get set }
-    
-    /// The maximum number of attempts that this operation should make before completely aborting. This value is nil when there is no maximum.
-    var maxRecoveryAttempts: UInt? { get }
-}
-
-// MARK: - Recoverable Default Implementations
-public extension Recoverable {
-    
-    /// The ability of this operation to recover from the last encountered failure.
-    var isRecoverable: Bool {
-        return maxRecoveryAttempts.map { recoveryAttemptCount < $0 } ?? true
-    }
-    
-    /// Creates a new copy of the operation ready for a new attempt to be made.
-    ///
-    /// - Returns: A new `Self`, with updated recoverability information.
-    func updatedForNextAttempt() -> Self? {
-        guard isRecoverable else { return nil }
-        var copy = self
-        copy.recoveryAttemptCount += 1
-        return copy
-    }
-}
-
 // MARK: - RecoveryDisposition
 
 /// The action to take in response to a recovery attempt.
